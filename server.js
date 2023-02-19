beb0886e56d406a0d541740471f41bb76a724d06
sbohmann <mail@sebastian-bohmann.com>
2023-02-19 16:54:28 +0100

    Photo upload

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const fs = require('fs')
 
 http
     .createServer((request, response) => {
-        console.log(request)
-        response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
+        if (request.url === '/upload') {
+            console.log('UPLOAD!!!')
+            response.end()
+        } else {
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
 
-        let readStream = fs.createReadStream('static/photoupload.html');
-        readStream.pipe(response);
+            let readStream = fs.createReadStream('static/photoupload.html');
+            readStream.pipe(response);
+        }
     })
     .listen(5000)
